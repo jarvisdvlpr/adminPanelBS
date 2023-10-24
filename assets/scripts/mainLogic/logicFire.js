e8b53c676710e95741d1efd75b71f2bb2a04fa01
LENOVO <azizka.ibragimov@gmail.com>
2023-10-24 13:36:29 +0400

    finished

--- assets/scripts/mainLogic/logicFire.js
+++ assets/scripts/mainLogic/logicFire.js
@@ -10,8 +10,11 @@ const bookImgUrlInput = document.getElementById("Burl");
 const bookDescriptionInput = document.getElementById("dscrptn");
 const bookCategoryInput = document.getElementById("bCategory");
 
+
 let books = [];
 let categories = [];
+let contacts = [];
+
 
 
 function cleanBookForm() {
@@ -41,7 +44,7 @@ function showSuccess(alertMessage) {
 
 
 function showBooks(myBooks){
-   let booksTable = document.getElementById("booksTable");
+   let booksTable = document.getElementById("books-tbody");
    if(myBooks === undefined || myBooks === null || myBooks.length === 0){
       return;
    }
@@ -49,26 +52,73 @@ function showBooks(myBooks){
       booksTable.removeChild(booksTable.lastChild);
    }
 
-   booksTable.insertAdjacentHTML('beforeend', `<div class="head">
-            <span>#</span>
-            <span>Title</span>
-            <span>Description</span>
-            <span>Category</span>
-            <span>Author</span>
-          </div>`);
+
+
    for (let i in myBooks) {
       booksTable.insertAdjacentHTML('beforeend',
-          `
-            <div class="info">
-            <span>${Number(i)+1}</span>
-            <span>${myBooks[i].name}</span>
-            <span>${myBooks[i].description}</span>
-            <span>${myBooks[i].category}</span>
-            <span>${myBooks[i].author}</span>
-          </div>`)
+          `<tr id="bookId-${i}">
+              <td>${Number(i)+1}</td>
+              <td>${myBooks[i].name}</td>
+              <td>${myBooks[i].description.substring(0,100)}...</td>
+              <td>${myBooks[i].category}</td>
+              <td>${myBooks[i].author}</td>
+              <td>
+              <i class="fa-regular fa-trash-can" id="book-tab-del-${i}" style="cursor: pointer"</i>
+              </td>
+            </tr>`);
    }
 }
 
+function deleteBooks(bookList) {
+   for (let i in bookList) {
+      document.getElementById(`book-tab-del-${i}`).addEventListener("click", ()=>{
+         bookList.splice(i, 1);
+         set(ref(db, '/books'), bookList);
+         document.getElementById(`bookId-${i}`).remove();
+      });
+   }
+}
+
+
+
+function showContactUs(contactList) {
+   const contactTable = document.getElementById("contact-tbody");
+   if(contacts === undefined || contacts === null || contacts.length === 0){
+      return;
+   }
+
+   while (contactTable.firstChild) {
+      contactTable.removeChild(contactTable.lastChild);
+   }
+
+
+   for (let i in contactList) {
+      contactTable.insertAdjacentHTML('beforeend',
+          `<tr id="contactId-${i}">
+              <td>${Number(i)+1}</td>
+              <td>${contactList[i].fullName}</td>
+              <td>${contactList[i].address}</td>
+              <td>${contactList[i].emailAddress}</td>
+              <td>${contactList[i].phoneNumber}</td>
+              <td>${contactList[i].note}</td>
+              <td><i class="fa-regular fa-trash-can" id="contact-tab-del-${i}" style="cursor: pointer"></i></td>
+            </tr>`);
+   }
+
+
+}
+
+function addContactUsDeleteEventListeners(contactList) {
+   for (let i in contactList) {
+      document.getElementById(`contact-tab-del-${i}`).addEventListener("click", ()=>{
+         contactList.splice(i, 1);
+         set(ref(db, '/contactus'), contactList);
+
+         document.getElementById(`contactId-${i}`).remove();
+      })
+   }
+
+}
 function showCategories(cats) {
    let selectCatTag = document.getElementById("bCategory");
    if(cats===undefined || cats === null || cats.length ===0){
@@ -91,6 +141,8 @@ function showCategories(cats) {
    }
 }
 
+
+
 window.addEventListener("load", (event) => {
   get(ref(db, '/categories')).then(snapshot=>{
      const oldCategories = snapshot.val();
@@ -103,13 +155,57 @@ window.addEventListener("load", (event) => {
      }
   })
 
-   get(ref(db, '/books'), books).then(snapshot=>{
+   get(ref(db, '/books')).then(snapshot=>{
       const oldBooks = snapshot.val();
       if(oldBooks!==null){
          books = oldBooks;
          showBooks(books);
+         deleteBooks(books);
       }else{
          showBooks([])
+      }
+   });
+
+  get(ref(db, '/contactus')).then(snapshot=>{
+     const oldContacts  = snapshot.val();
+     if(oldContacts !== null){
+        contacts = oldContacts;
+        showContactUs(contacts);
+        addContactUsDeleteEventListeners(contacts)
+     }else{
+        showContactUs([])
+     }
+  });
+
+   get(ref(db, '/aboutUs')).then(snapshot =>{
+      const aboutCurrent = snapshot.val();
+      if(aboutCurrent!== null){
+         document.getElementById("titleAbout").value = aboutCurrent.title;
+         document.getElementById("descAboutUs").value = aboutCurrent.description;
+         document.getElementById("imgUrlAbout").value = aboutCurrent.imgSrc;
+      }
+   });
+
+   get(ref(db, '/joinUs')).then(snapshot=>{
+      const joinArray = snapshot.val();
+      if(joinArray !== null){
+         for (let i in joinArray) {
+            document.getElementById("join-tbody").insertAdjacentHTML(
+                'beforeend',
+                `<tr id="joinId-${i}">
+              <td>${Number(i)+1}</td>
+              <td>${joinArray[i].fullName}</td>
+              <td>${joinArray[i].emailAddress}</td>
+              <td><i class="fa-regular fa-trash-can"id="join-tab-del-${i}" style="cursor: pointer"></i></td>
+
+            </tr>`);
+
+            document.getElementById(`join-tab-del-${i}`).addEventListener("click", ()=>{
+               joinArray.splice(i, 1);
+               set(ref(db, '/joinUs'), joinArray);
+               document.getElementById(`joinId-${i}`).remove();
+            })
+         }
       }
    });
 
@@ -231,4 +327,14 @@ document.getElementById("makeSearchGoogle").addEventListener("click", (e)=>{
 
 document.querySelector(".logOut").addEventListener("click", ()=>{
    location.replace("index.html");
+});
+
+document.getElementById("aboutUsForm").addEventListener("submit", (e)=>{
+   e.preventDefault()
+   const aboutUs = new myClasses.AboutUs(document.getElementById("titleAbout").value,
+       document.getElementById("descAboutUs").value, document.getElementById("imgUrlAbout").value);
+
+   set(ref(db, "/aboutUs"), aboutUs);
+
+   showSuccess("About us has been edited!")
 });
